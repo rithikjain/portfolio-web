2577a64a95a474a857c32ecf992798585aff5c7a
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-21 18:14:03 +0530

    feat: tech stack page done

--- src/pages/Tech.jsx
+++ src/pages/Tech.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
-
+import React from "react";
+import { AiFillAndroid } from "react-icons/ai";
+import { RiFlutterFill } from "react-icons/ri";
+import { SiKotlin } from "react-icons/si";
+import { FaNodeJs, FaReact } from "react-icons/fa";
+import GoLang from "../assets/golang.svg";
+ 
 const Tech = () => {
   return (
-    <div>
-      <h1>TECH STACK PAGE</h1>
-    </div>
-  )
-}
+    <div className="h-full flex items-center justify-center">
+      <div className="w-8/12 h-3/5 grid grid-cols-3 grid-rows-2 items-center">
+        {/* Android */}
+        <div className="inline-flex flex-col items-center">
+          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
+            <AiFillAndroid color="white" size="80" />
+          </div>
+          <span className="font-bold mt-2">Android</span>
+        </div>
 
-export default Tech
+        {/* Flutter */}
+        <div className="inline-flex flex-col items-center">
+          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
+            <RiFlutterFill color="white" size="80" className="mr-2" />
+          </div>
+          <span className="font-bold mt-2">Flutter</span>
+        </div>
+
+        {/* GoLang */}
+        <div className="inline-flex flex-col items-center">
+          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
+            <img src={GoLang} alt="GoLang" width="55%"/>
+          </div>
+          <span className="font-bold mt-2">GoLang</span>
+        </div>
+
+        {/* Kotlin */}
+        <div className="inline-flex flex-col items-center">
+          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
+            <SiKotlin color="white" size="60" />
+          </div>
+          <span className="font-bold mt-2">Kotlin</span>
+        </div>
+
+        {/* NodeJS */}
+        <div className="inline-flex flex-col items-center">
+          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
+            <FaNodeJs color="white" size="80" />
+          </div>
+          <span className="font-bold mt-2">NodeJS</span>
+        </div>
+
+        {/* React */}
+        <div className="inline-flex flex-col items-center">
+          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
+            <FaReact color="white" size="80" />
+          </div>
+          <span className="font-bold mt-2">React</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tech;
