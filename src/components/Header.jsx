8b1fb4b0660de42714fb28704d47a2d364490fb3
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-25 15:34:51 +0530

    fix(header): made it slightly more responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,31 +8,33 @@ const Header = () => {
   return (
     <div>
       <div className="flex items-center justify-between w-11/12 mx-auto py-8">
-        <h1 className="text-lg sm:text-2xl font-semibold tracking-wider">RITHIK JAIN</h1>
-        <div className="flex space-x-4 sm:space-x-8">
+        <h1 className="text-lg sm:text-2xl font-semibold tracking-wider">
+          RITHIK JAIN
+        </h1>
+        <div className="flex space-x-4 sm:space-x-8 items-center">
           <a href="mailto:rithik.jain3006@gmail.com">
-            <img src={email} alt="Email" height="25px" width="25px" />
+            <img src={email} alt="Email" className="w-5 sm:w-6" />
           </a>
           <a
             href="https://t.me/rithikj"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={telegram} alt="Telegram" height="25px" width="25px" />
+            <img src={telegram} alt="Telegram" className="w-5 sm:w-6" />
           </a>
           <a
             href="https://www.linkedin.com/in/rithik-jain-710b3a199/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={linkedin} alt="LinkedIn" height="25px" width="25px" />
+            <img src={linkedin} alt="LinkedIn" className="w-5 sm:w-6" />
           </a>
           <a
             href="https://www.github.com/rithikjain"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={github} alt="GitHub" height="25px" width="25px" />
+            <img src={github} alt="GitHub" className="w-5 sm:w-6" />
           </a>
         </div>
       </div>
