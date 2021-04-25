45747944b1eca3aa78ae015aeb270cdfc766103e
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-25 14:51:20 +0530

    fix(tech): made it responsive

--- src/pages/Tech.jsx
+++ src/pages/Tech.jsx
@@ -20,53 +20,53 @@ const Tech = ({ setPage, pageVariants, pageTransition }) => {
       variants={pageVariants}
       transition={pageTransition}
     >
-      <div className="w-8/12 gap-y-20 grid grid-cols-3 grid-rows-2 items-center">
+      <div className="w-8/12 gap-y-10 gap-x-16 lg:gap-y-20 grid grid-cols-3 grid-rows-2 items-center">
         {/* Android */}
         <div className="inline-flex flex-col items-center">
-          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
-            <AiFillAndroid color="white" size="80" />
+          <div className="bg-black w-20 h-20 lg:w-36 lg:h-36 rounded-full flex items-center justify-center">
+            <AiFillAndroid color="white" size="70%" />
           </div>
-          <span className="font-bold mt-2">Android</span>
+          <span className="font-bold text-sm sm:text-base mt-2">Android</span>
         </div>
 
         {/* Flutter */}
         <div className="inline-flex flex-col items-center">
-          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
-            <RiFlutterFill color="white" size="80" className="mr-2" />
+          <div className="bg-black w-20 h-20 lg:w-36 lg:h-36 rounded-full flex items-center justify-center">
+            <RiFlutterFill color="white" size="70%" className="mr-2" />
           </div>
-          <span className="font-bold mt-2">Flutter</span>
+          <span className="font-bold text-sm sm:text-base mt-2">Flutter</span>
         </div>
 
         {/* GoLang */}
         <div className="inline-flex flex-col items-center">
-          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
+          <div className="bg-black w-20 h-20 lg:w-36 lg:h-36 rounded-full flex items-center justify-center">
             <img src={GoLang} alt="GoLang" width="55%" />
           </div>
-          <span className="font-bold mt-2">GoLang</span>
+          <span className="font-bold text-sm sm:text-base mt-2">GoLang</span>
         </div>
 
         {/* Kotlin */}
         <div className="inline-flex flex-col items-center">
-          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
-            <SiKotlin color="white" size="60" />
+          <div className="bg-black w-20 h-20 lg:w-36 lg:h-36 rounded-full flex items-center justify-center">
+            <SiKotlin color="white" size="45%" />
           </div>
-          <span className="font-bold mt-2">Kotlin</span>
+          <span className="font-bold text-sm sm:text-base mt-2">Kotlin</span>
         </div>
 
         {/* NodeJS */}
         <div className="inline-flex flex-col items-center">
-          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
-            <FaNodeJs color="white" size="80" />
+          <div className="bg-black w-20 h-20 lg:w-36 lg:h-36 rounded-full flex items-center justify-center">
+            <FaNodeJs color="white" size="70%" />
           </div>
-          <span className="font-bold mt-2">NodeJS</span>
+          <span className="font-bold text-sm sm:text-base mt-2">NodeJS</span>
         </div>
 
         {/* React */}
         <div className="inline-flex flex-col items-center">
-          <div className="bg-black w-36 h-36 rounded-full flex items-center justify-center">
-            <FaReact color="white" size="80" />
+          <div className="bg-black w-20 h-20 lg:w-36 lg:h-36 rounded-full flex items-center justify-center">
+            <FaReact color="white" size="70%" />
           </div>
-          <span className="font-bold mt-2">React</span>
+          <span className="font-bold text-sm sm:text-base mt-2">React</span>
         </div>
       </div>
     </motion.div>
