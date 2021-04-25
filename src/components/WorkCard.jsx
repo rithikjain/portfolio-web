005a58ab99a72d0595e868b2f9a144970d16a606
Rithik Jain <rithik.jain3006@gmail.com>
2021-04-25 21:29:58 +0530

    fix(workcard): width of the card changed

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WorkCard = ({ company, position, duration, info1, info2 }) => {
   return (
     <div className="flex-1 h-full flex flex-col items-center justify-center mb-16">
-      <div className="w-10/12 flex flex-col">
+      <div className="w-11/12 xl:w-7/12 flex flex-col">
         <h1 className="font-bold text-3xl md:text-4xl">{company}</h1>
         <p className="ml-2 mt-4 text-sm md:text-lg text-gray-500 font-medium">
           {position}
